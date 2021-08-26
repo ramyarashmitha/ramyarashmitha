d7688b56e98f3063103738bafef37c157a4f77fa
ramyarashmitha <67183264+ramyarashmitha@users.noreply.github.com>
2021-08-26 16:21:49 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,11 @@
+# Python prgm to swap numbers
+
+x = 5
+y = 10
+
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
